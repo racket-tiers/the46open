3b2f70a397a9b0feee6c4be3b4f17dafe3e125ab
Michelle Bergquist <caker25@gmail.com>
2017-04-24 15:38:14 -0600

    Deploy db

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
-
 
 const linkQuery = require('./db/user-info')
 
-app.use('/', express.static('public'));
+app.use('/', express.static('public'))
 app.set('view engine', 'hbs')
-
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-	linkQuery.getAll()
+  linkQuery.getAll()
 	.then(data => {
-        res.render('index', {data})
-
-	})
+  res.render('index', {data})
+})
 })
 
-
-app.listen(port, function(){
-  console.log("Listening on local host " + port);
+app.listen(port, function () {
+  console.log('Listening on local host ' + port)
 })
