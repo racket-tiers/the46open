c2e1352faf31dbc7f2e708752663df1d96d1dfef
Jules Perryman <jules.perryman@gmail.com>
2017-04-24 15:11:37 -0600

    Add ratings

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const app = express()
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
 
+
+const linkQuery = require('./db/user-info')
+
 app.use('/', express.static('public'));
 app.set('view engine', 'hbs')
 
-app.get('/',(req,res)=>{
-  res.render('index')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+	linkQuery.getAll()
+	.then(data => {
+        res.render('index', {data})
+
+	})
 })
+
 
 app.listen(port, function(){
   console.log("Listening on local host " + port);
