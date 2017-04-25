f56026cb16eb19b0e7275d5d773f0d383b5cd1c4
James Schultz <ninjames101@gmail.com>
2017-04-25 11:32:32 -0600

    created and populated rules page

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
 
+app.get('/rules', (req, res) => {
+  res.render('rules')
+})
+
 app.listen(port, function () {
   console.log('Listening on local host ' + port)
 })
