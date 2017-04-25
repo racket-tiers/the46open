3c665216276c97f9f59c022c9c8c458de56e9819
Michelle Bergquist <caker25@gmail.com>
2017-04-25 14:30:21 -0600

    Start login route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
 const port = process.env.PORT || 3000
+const pg = require('./db/knex')
 
 const linkQuery = require('./db/user-info')
 
@@ -11,24 +12,24 @@ app.use('/', express.static('public'))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }))
 
 app.use(bodyParser.json())
 
+// GENERATES THE LEADERBOAD ON THE MAIN PAGE
 app.get('/', (req, res) => {
-    linkQuery.getAll()
+  linkQuery.getAll()
         .then(data => {
-            res.render('index', {
-                data
-            })
+          res.render('index', {
+            data
+          })
         })
 })
 
 app.get('/createAccount', (req, res) => {
-    res.render('createAccount')
+  res.render('createAccount')
 })
-
 
 app.get('/about', (req, res) => {
   res.render('about')
@@ -40,7 +41,13 @@ app.get('/rules', (req, res) => {
 
 app.listen(port, function () {
   console.log('Listening on local host ' + port)
+})
 
+// LOG IN TO ACCOUNT
+app.post('/profile', (req, res) => {
+  pg('user_table').select().where('email', req.query.email).then((data) => {
+    res.render('profile', {data})
+  })
 })
 
 // app.post('/createAccount', function(req, res, next) {
