367302a29eb096c29e3f852a8ff50099c534693a
Jules Perryman <jules.perryman@gmail.com>
2017-04-25 13:05:13 -0600

    set up for password - login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 const port = process.env.PORT || 3000
 
 const linkQuery = require('./db/user-info')
@@ -9,21 +10,56 @@ const linkQuery = require('./db/user-info')
 app.use('/', express.static('public'))
 app.set('view engine', 'hbs')
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  linkQuery.getAll()
-	.then(data => {
-  res.render('index', {data})
-})
+    linkQuery.getAll()
+        .then(data => {
+            res.render('index', {
+                data
+            })
+        })
 })
 
 app.get('/createAccount', (req, res) => {
-  res.render('createAccount')
+    res.render('createAccount')
 })
 
-app.listen(port, function () {
-  console.log('Listening on local host ' + port)
+app.listen(port, function() {
+    console.log('Listening on local host ' + port)
 })
+
+// app.post('/createAccount', function(req, res, next) {
+//     queries.findUserIfExists().where({
+//             email: req.body.email
+//         }).first()
+//         .then(function(user) {
+//                 if (user) {
+//                     res.send(user)
+//                 } else {
+//                     bcrypt.genSalt(8, function(err, salt) {
+//                         console.log(salt);
+//                         bcrypt.hash(req.body.password, sal, function(err, hash) {
+//                             console.log(hash);
+//                             hashedPassword = hash
+//                             porcess.exit()
+//                         })
+//                         }).then(function() {
+//                             queries.userTable(req.body, hashedPassword)
+//                                 .then(function() {
+//                                     res.send("new user!")
+//                                 })
+//                             .catch(function(err){
+//                                 console.log(err);
+//                             })
+//                     })
+//
+//                 }
+//
+//
+//             }
+//         })
---
+++ db/query.js
@@ -0,0 +1,17 @@
+// var pg = require('/knex')
+//
+// function getAll(){
+//     return pg('post').select()
+// }
+//
+// function add(obj){
+//     return pg('post').insert(obj)
+// }
+//
+// function findUserIfExists(obj){
+//     return pg('user_table').select()
+// }
+//
+// function userTable(obj, hashedPassword){
+//     return('myusers')
+// }
