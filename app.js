ea6b9c5ff71218ecdf422ca66d78b632df29bff7
Michelle Bergquist <caker25@gmail.com>
2017-04-27 09:27:49 -0600

    Add delete work

--- app.js
+++ app.js
@@ -112,20 +112,18 @@ app.get('/account/:id', (req, res) => {
 })
 
 // DELETE ACCOUNT, FROM ACCOUNT SETTINGS PAGE
-// app.delete('/remove', (req, res) => {
-//   linkQuery.seeIfUserExists().where({
-//     id: req.params.id
-//   }).first().then(function (data) {
-//
-//   ///////////////////////////////////
-//   pg('user_table')
-//   // TODO: COOKIE STUFF HERE
-//   .where('id', 400)
-//   .del()
-//   .then(() => {
-//     res.redirect('/')
-//   })
-// })
+app.delete('/remove', (req, res) => {
+  linkQuery.seeIfUserExists().where({
+    id: req.params.id
+  }).first().then(function (data) {
+    pg('user_table')
+  .where('id', data.id)
+  .del()
+  .then(() => {
+    res.redirect('/')
+  })
+  })
+})
 
 // UPDATE ACCOUNT, FROM ACCOUNT SETTINGS PAGE
 // app.put('/update', (req, res) => {
