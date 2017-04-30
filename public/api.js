7355351e6cd2c6260bd4f0d3208f748deddb4439
Michelle Bergquist <caker25@gmail.com>
2017-04-29 18:48:47 -0600

    Add API
    Add API with JS; load on main page footer

---
+++ public/api.js
@@ -0,0 +1,13 @@
+const req = new XMLHttpRequest()
+const url = 'http://quotes.stormconsultancy.co.uk/random.json'
+req.open('GET', url)
+req.onload = () => {
+  var apiObj = JSON.parse(req.responseText)
+  var quote = apiObj.quote
+  var footerEl = document.querySelector('footer')
+  footerEl.textContent = quote
+}
+req.onerror = () => {
+  document.querySelector('footer').textContent = 'Api not loading.'
+}
+req.send()
